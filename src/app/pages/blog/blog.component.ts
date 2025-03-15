169bad42949549b0b47b1422a5267000c8ae8c02
Fabricio Murillo Flores <fabriciomf72@gmail.com>
2025-03-15 14:16:04 -0600

    update blog component

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ArticleListComponent } from "../../components/article-list/article-list.component";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [ArticleListComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
